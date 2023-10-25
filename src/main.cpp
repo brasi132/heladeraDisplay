d5431d8c310e4129da30e7f5adc1a266c279e600
brasi132 <tomas.figueroa132@outlook.com>
2023-10-25 15:53:27 -0300

    adc

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 #include <LiquidCrystal_I2C.h>
 #include <OneWire.h>
 
-OneWire ds(2);  // Pin de datos del sensor DS18B20
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Dirección del I2C y tamaño del LCD
+OneWire ds(2);  
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
 byte addr[8]; 
 
 void setup() {
-  lcd.init();  // Inicializa el LCD
+  lcd.init();  
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Temperatura:");
@@ -17,17 +17,17 @@ void setup() {
 void loop() {
   lcd.setCursor(0, 1);
   
-  // Lee la temperatura del sensor DS18B20
+  
   float tempC;
   if (ds.search(addr)) {
     ds.reset();
     ds.select(addr);
-    ds.write(0x44, 1);  // Inicia la conversión de temperatura
-    delay(800);  // Espera a que la conversión termine
+    ds.write(0x44, 1);  
+    delay(800);  
     
     ds.reset();
     ds.select(addr);
-    ds.write(0xBE);  // Lee el registro de datos
+    ds.write(0xBE);  
     
     byte data[9];
     for (int i = 0; i < 9; i++) {
@@ -41,5 +41,5 @@ void loop() {
   lcd.print(tempC);
   lcd.print(" C");
 
-  delay(400);  // Actualiza la temperatura cada segundo
+  delay(400);  
 }
